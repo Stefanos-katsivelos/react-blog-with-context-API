75cf12217171177e5b5698fa8f08f54d2c151f5e
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-16 07:51:26 +0300

    passed values to child components with context

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Post from "./Post";
+import UserInfoContext from "../context/UserInfoContext";
 export default function BlogPage() {
+  const userInfo = useContext(UserInfoContext);
+
   return (
     <div>
       <h1>Blog</h1>
-      <Post></Post>
+      <Post username={userInfo.username} isAdmin={userInfo.isAdmin}></Post>
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Comment from "./Comment";
-export default function Post() {
+
+export default function Post({ username, isAdmin }) {
   return (
     <div>
+      {isAdmin && <button>Delete</button>}
       <h1>Example Post Title</h1>
       <p>This is an example post content.</p>
 
-      <Comment></Comment>
+      <Comment username={username} isAdmin={isAdmin}></Comment>
     </div>
   );
 }
